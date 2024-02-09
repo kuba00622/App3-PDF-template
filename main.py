fd8bb2a31ac9b1c43e6e7e894e214d8a4122bb59
Kuba <jkalamaszek@gmail.com>
2024-02-09 20:37:02 +0100

    Add footers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fpdf.enums import XPos, YPos
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -15,8 +16,21 @@ for index, row in df.iterrows():
              align="L", new_x=XPos.LMARGIN, new_y=YPos.NEXT,
              border=0)
     pdf.line(10, 21, 200, 21)
+
+    pdf.ln(265)
+
+    pdf.set_font(family="Times", style="B", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=8, text=row['Topic'], align="R")
+
     for i in range(row['Pages']-1):
         pdf.add_page()
+
+        pdf.ln(277)
+
+        pdf.set_font(family="Times", style="B", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=8, text=row['Topic'], align="R")
 
 
 
